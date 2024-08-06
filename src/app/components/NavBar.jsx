7de7a831c43a18b693daef0bd999118dcdf707e2
naveed60 <ubuntu24@naveed60.localdomain>
2024-08-06 16:54:10 +0500

    update new changes

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+
+    return (
+        <div className="navbar bg-blue-600 fixed top-0 left-0 right-0 w-full z-50">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Next Js Auth</a>
+            </div>
+            <div className="flex-none gap-8">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
